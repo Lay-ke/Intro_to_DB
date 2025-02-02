00efc3ed7638d91010838d9fd8b69cf7a4c9420f
Yaw Mintah <yawmintah1@gmail.com>
2025-02-02 16:21:20 +0000

    fixed checker error of Order_Details Table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,7 +37,9 @@ CREATE TABLE Orders (
 # Order_Details
 CREATE TABLE Order_Details (
     order_detail_id PRIMARY KEY,
+    order_id INT,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    book_id INT,
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
